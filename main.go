17b4365fdf11af124be50ebcc73a3f74afff9150
Fabricio Elizondo Fernández <fabelifer@gmail.com>
2021-06-10 11:55:36 -0600

    [Add] Dockerfile

--- main.go
+++ main.go
@@ -359,7 +359,7 @@ func failOnError(err error, msg string) {
 func brokerListening(){
 
 	var newBrokerMessage brokerMessage
-	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	conn, err := amqp.Dial("amqp://guest:guest@rabbitmq-server:5672/")
 	failOnError(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
 
